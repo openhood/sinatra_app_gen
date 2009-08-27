29c6b1fd1ae0f5ce24bb50284d1fad8f6f7f141b
Joseph HALTER <joseph.halter@akiraweb.lu>
2009-08-27 11:40:52 +0200

    First commit

---
+++ Rakefile
@@ -0,0 +1,20 @@
+require "rubygems"
+require "rake"
+
+begin
+  require "jeweler"
+  Jeweler::Tasks.new do |gem|
+    gem.name = "sinatra_app_gen"
+    gem.summary = "Creates a new Sinatra / Sequel / rspec / cucumber app."
+    gem.description = "Our default app template"
+    gem.email = "team@openhood.com"
+    gem.homepage = "http://github.com/openhood/sinatra_app_gen"
+    gem.authors = ["Jonathan Tron", "Joseph Halter"]
+    gem.add_dependency "rspec", "~> 1.2.6"
+    gem.add_dependency "cucumber", "~> 0.3.97"
+    gem.add_dependency "sequel", ">= 3.2.0"
+    # gem is a Gem::Specification... see http://www.rubygems.org/read/chapter/20 for additional settings
+  end
+rescue LoadError
+  puts "Jeweler (or a dependency) not available. Install it with: sudo gem install jeweler"
+end
---
+++ lib/generators/sinatra_app/sinatra_app_generator.rb
@@ -0,0 +1,63 @@
+class SinatraAppGenerator < RubiGen::Base
+  DEFAULT_SHEBANG = File.join(Config::CONFIG['bindir'],
+                              Config::CONFIG['ruby_install_name'])
+
+  attr_reader :name
+
+  def initialize(runtime_args, runtime_options = {})
+    super
+    usage if args.empty?
+    @destination_root = File.expand_path(args.shift)
+    @name = base_name
+  end
+
+  def template(m, filename)
+    m.template "#{filename}.erb", filename
+  end
+
+  def manifest
+    record do |m|
+      # Ensure appropriate folder(s) exists
+      m.directory ''
+      BASEDIRS.each { |path| m.directory path }
+
+      # root
+      template m, "app.rb"
+      template m, "config.ru"
+      template m, "geminstaller.yml"
+      template m, "Rakefile"
+
+      # cucumber
+      m.directory "features/support"
+      m.directory "features/step_definitions"
+      template m, "features/support/env.rb"
+      template m, "features/support/paths.rb"
+      template m, "features/step_definitions/webrat_steps.rb"
+
+      # rspec
+      m.directory "spec"
+      template m, "spec/spec_helper.rb"
+
+      # lib
+      m.directory "lib"
+      template m, "lib/rack_fix.rb"
+    end
+  end
+
+  protected
+    def banner
+      <<-EOS
+Creates a new Sinatra / Sequel / rspec / cucumber app.
+
+USAGE: #{spec.name} module_name
+
+EOS
+    end
+
+    # Installation skeleton.  Intermediate directories are automatically
+    # created so don't sweat their absence here.
+    BASEDIRS = %w(
+      lib
+      spec
+    )
+end
